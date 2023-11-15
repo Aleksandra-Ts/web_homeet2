e624251f5bfae8e2ad7a39f0a9c517f0b973c1d9
Aleksandra-Ts <96833111+Aleksandra-Ts@users.noreply.github.com>
2023-11-15 22:42:33 +0300

    Update views.py

--- django_project/myapp/views.py
+++ django_project/myapp/views.py
@@ -1,7 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from . import models  
+import json
+from .models import User
+
 
 def index(request):
+    global name_tek
+    name_tek = ''
+    if request.method == "POST":
+        data = request.POST
+        data = dict(data)
+        print(data)
+        person = User()
+        person.name = *request.POST.get("name")
+        name_tek = person.name
+        person.gender = *request.POST.get("gender")
+        person.age = *request.POST.get("date")
+        person.telegram = *request.POST.get("tg")
+        person.number = *request.POST.get("tel")
+        person.text_about = *request.POST.get("textarea")
+        person.save()
     return render(request, 'myapp/index.html', {})
 
 def index2(request):
-    return render(request, 'myapp/index2.html', {})
+    global name_tek
+    person = Person.objects.get(name=name_tek)
+
+    if request.method == "POST":
+        data = request.POST
+        data = dict(data)
+        print(data)
+        person.is_student = *request.POST.get("name")
+        person.course = *request.POST.get("gender")
+        person.is_ended = *request.POST.get("date")
+        person.step = *request.POST.get("tg")
+        person.faculty = *request.POST.get("tel")
+        person.program = *request.POST.get("textarea")
+        person.job = *request.POST.get("")
+        person.save()
+    return render(request, 'myapp/index2.html', {'person': person})
